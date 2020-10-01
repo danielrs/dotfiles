e9b54ae2d5c3419a99ffa13c50bc555f8b59b9c9
Daniel Rivas <danielrs@users.noreply.github.com>
2020-10-01 13:57:11 -0500

    Simpler ksh.fish

--- dot_config/fish/functions/ksh.fish
+++ dot_config/fish/functions/ksh.fish
@@ -1,8 +1,4 @@
-function ksh --description 'Alias for kitty +kitten ssh, defaults to ssh if not running Kitty'
-    if [ "$TERM" = "xterm-kitty" ]
-        kitty +kitten ssh $argv
-    else
-        command ssh $argv
-    end
+function ksh --description 'Alias for kitty +kitten ssh'
+    kitty +kitten ssh $argv
 end
 
