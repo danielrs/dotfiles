54bdd396e96c54b5c56ea6b38e32bb842593fc88
Daniel Rivas <danielrs@users.noreply.github.com>
2020-10-01 13:59:05 -0500

    ksh defaults to ssh if not running on kitty

--- dot_config/fish/functions/ksh.fish
+++ dot_config/fish/functions/ksh.fish
@@ -1,4 +1,8 @@
-function ksh --description 'Alias for kitty +kitten ssh'
-    kitty +kitten ssh $argv
+function ksh --description 'Alias for kitty +kitten ssh, defaults to ssh if not running kitty'
+    if [ "$TERM" = "xterm-kitty" ]
+        kitty +kitten ssh $argv
+    else
+        command ssh $argv
+    end
 end
 
