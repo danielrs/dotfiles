530a20d21ca9a7ae861a0e587e779d2479e33fbb
Daniel Rivas <danielrs@users.noreply.github.com>
2020-10-01 13:52:46 -0500

    Typo in ksh.fish

--- dot_config/fish/functions/ksh.fish
+++ dot_config/fish/functions/ksh.fish
@@ -1,4 +1,4 @@
-function ssh --description 'Alias for kitty +kitten ssh, defaults to regular ssh if not running Kitty'
+function ksh --description 'Alias for kitty +kitten ssh, defaults to ssh if not running Kitty'
     if [ "$TERM" = "xterm-kitty" ]
         kitty +kitten ssh $argv
     else
