f5d7186a94e9513e07d58cc038528a2fe9ce8a5f
Daniel Rivas <1887507+danielrs@users.noreply.github.com>
2023-07-12 10:32:01 -0500

    Export Fish COLUMNS env var

---
+++ dot_config/fish/conf.d/columns.fish
@@ -0,0 +1,3 @@
+# Some CLI tools work better if they have the COLUMNS environment variable available.
+# See: http://pubs.opengroup.org/onlinepubs/009604499/basedefs/xbd_chap08.html
+export COLUMNS=$COLUMNS
