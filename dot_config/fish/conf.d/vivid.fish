ce8d91610ab87675da1edffdbf405ab02ede4c0f
Daniel Rivas <1887507+danielrs@users.noreply.github.com>
2025-09-24 01:18:31 -0500

    Setup and use Catppuccin Latte color scheme

--- dot_config/fish/conf.d/vivid.fish
+++ dot_config/fish/conf.d/vivid.fish
@@ -1,5 +1,6 @@
 # Configure LS_COLORS for apps that use it (ls, find, sd, fd, etc).
 # See: https://github.com/sharkdp/vivid
 if type --query vivid
-  export LS_COLORS="$(vivid generate solarized-light)"
+  # export LS_COLORS="$(vivid generate solarized-light)"
+  export LS_COLORS="$(vivid generate catppuccin-latte)"
 end
